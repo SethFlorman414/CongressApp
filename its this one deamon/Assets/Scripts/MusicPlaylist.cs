dca80c91e0cf3eb41a637b11350b1569ec886087
Seth Florman <18.seth.florman@ksd.org>
2018-01-16 11:41:52 -0800

    Music Playlist

---
+++ its this one deamon/Assets/Scripts/MusicPlaylist.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlaylist : MonoBehaviour {
+
+	public AudioClip[] clips;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = FindObjectOfType<AudioSource> ();
+		audioSource.loop = false;
+		
+	}
+
+	private AudioClip GetRandomClip(){
+		return clips [Random.Range (0, clips.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!audioSource.isPlaying) {
+			audioSource.clip = GetRandomClip ();
+			audioSource.Play ();
+		}
+
+	}
+}
